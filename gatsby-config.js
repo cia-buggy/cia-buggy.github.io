acf4698956f4f6d7364237708e237c7b5e8d974a
Erin <yane@bxscience.edu>
2022-03-20 15:35:45 -0400

    moved site removed node and cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,20 @@
 module.exports = {
   siteMetadata: {
-      title: `new`,
+      title: `cia-buggy`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+	pathPrefix: "/Tang-erin.github.io",
+
+plugins: ["gatsby-plugin-styled-components",    
+					`gatsby-transformer-sharp`,
+    			`gatsby-plugin-sharp`,
+{
+	resolve: "gatsby-source-filesystem",
+	options: {
+		path: `${__dirname}/src/images/gallery`,
+		name: "album",
+	},
+},
+]
+
 };
