43b7e20fcfaddb1bafaa43b8e4efb8de9974595b
Erin <yane@bxscience.edu>
2022-03-20 16:09:16 -0400

    fixed package and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
   siteMetadata: {
       title: `cia-buggy`,
-    siteUrl: `https://www.yourdomain.tld`
   },
-	pathPrefix: "/Tang-erin.github.io",
 
 plugins: ["gatsby-plugin-styled-components",    
 					`gatsby-transformer-sharp`,
