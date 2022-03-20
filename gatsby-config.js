c34071568cfb8941bd0bda66d1a32483e9f45b9f
Erin <yane@bxscience.edu>
2022-03-20 16:21:02 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,19 @@ module.exports = {
   siteMetadata: {
       title: `cia-buggy`,
   },
+	pathPrefix: "/",
 
-plugins: ["gatsby-plugin-styled-components",    
-					`gatsby-transformer-sharp`,
-    			`gatsby-plugin-sharp`,
-{
-	resolve: "gatsby-source-filesystem",
-	options: {
-		path: `${__dirname}/src/images/gallery`,
-		name: "album",
-	},
-},
-]
+	plugins: [
+		"gatsby-plugin-styled-components",    
+		`gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/images/gallery`,
+				name: "album",
+			},
+		},
+	]
 
 };
