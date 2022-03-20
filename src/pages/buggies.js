5d25ef511d901fd6e2bcfb543ab95e0c98fcab5d
Erin <yane@bxscience.edu>
2022-03-20 15:59:52 -0400

    capitalizaton

--- src/pages/buggies.js
+++ src/pages/buggies.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import styled from "styled-components"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import tempest from "../images/tempest.jpg"
-import equinox from "../images/equinox.jpg"
-import icarus from "../images/icarus.jpg"
-import orca from "../images/orca.jpg"
-import impulse from "../images/impulse.jpg"
-import kingfisher from "../images/kingfisher.jpg"
-import emperor from "../images/emperor.jpg"
-import aroura from '../images/aurora.jpg'
+import Tempest from "../images/tempest.jpg"
+import Equinox from "../images/equinox.jpg"
+import Icarus from "../images/icarus.jpg"
+import Orca from "../images/orca.jpg"
+import Impulse from "../images/impulse.jpg"
+import Kingfisher from "../images/kingfisher.jpg"
+import Emperor from "../images/emperor.jpg"
+import Aroura from '../images/aurora.jpg'
 import Navbar from "../components/navbar";
 import Footer from "../components/footer"
 import line from "../images/yellowline.png"
@@ -65,17 +65,17 @@ export default class buggycarosel extends Component {
 					<Wrapper>
 						<HeadingText>Active Buggies</HeadingText>
             <Carousel autoPlay="true" infiniteLoop="true">
-								<Buggyimg src={kingfisher}/>
-								<Buggyimg src={aroura}/>
-								<Buggyimg src ={emperor}/>
-                <Buggyimg src={tempest}/>
-                <Buggyimg src={equinox}/>
-                <Buggyimg src={icarus}/>
+								<Buggyimg src={Kingfisher}/>
+								<Buggyimg src={Aroura}/>
+								<Buggyimg src ={Emperor}/>
+                <Buggyimg src={Tempest}/>
+                <Buggyimg src={Equinox}/>
+                <Buggyimg src={Icarus}/>
             </Carousel>
 					<HeadingText>Retired Buggies</HeadingText>
 					 <Carousel autoPlay="true" infiniteLoop="true">
-                <Buggyimg src={impulse}/>
-                <Buggyimg src={orca}/>
+                <Buggyimg src={Impulse}/>
+                <Buggyimg src={Orca}/>
             </Carousel>
 					</Wrapper>
 					<Footer/>
